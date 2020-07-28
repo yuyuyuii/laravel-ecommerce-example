c6635341cd7700f7be1fb08197b6c34d12802e4c
y.maeda <yuyuyu.mae606@gmail.com>
2020-07-28 17:47:30 +0900

    商品とカテゴリを紐付けてテストデータ作成

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -20,7 +20,7 @@ class ProductsTableSeeder extends Seeder
           'details' => [13, 14,15][array_rand([13, 14,15])]. ' inch,'. [1, 2, 3][array_rand([1, 2, 3])]. ' TB SSD, 32GB RAM',
           'price' => rand(149999, 249999),
           'description' => 'mac book description, mac book description, mac book description'
-        ]);
+        ])->categories()->attach(1);//カテゴリテーブルを紐付け(attach(category_idを指定));
       }
 
       //Desktop
@@ -31,7 +31,7 @@ class ProductsTableSeeder extends Seeder
           'details' => [24, 25, 27][array_rand([24, 25, 27])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] . ' TB SSD, 32GB RAM',
           'price' => rand(249999, 449999),
           'description' => 'mac book description, mac book description, mac book description'
-        ]);
+        ])->categories()->attach(2);
       }
 
       // Phones
@@ -42,7 +42,7 @@ class ProductsTableSeeder extends Seeder
           'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [7, 8, 9][array_rand([7, 8, 9])] . ' inch screen, 4GHz Quad Core',
           'price' => rand(79999, 149999),
           'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-        ]);
+        ])->categories()->attach(3);
       }
 
         // Tablets
@@ -53,7 +53,7 @@ class ProductsTableSeeder extends Seeder
             'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [10, 11, 12][array_rand([10, 11, 12])] . ' inch screen, 4GHz Quad Core',
             'price' => rand(49999, 149999),
             'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-          ]);
+          ])->categories()->attach(4);
         }
         // TVs
         for ($i = 1; $i <= 9; $i++) {
@@ -63,7 +63,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => [46, 50, 60][array_rand([7, 8, 9])] . ' inch screen, Smart TV, 4K',
                 'price' => rand(79999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(5);
         }      
         
         // Cameras
@@ -74,7 +74,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => 'Full Frame DSLR, with 18-55mm kit lens.',
                 'price' => rand(79999, 249999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(6);
         }
         // Appliances
         for ($i = 1; $i <= 9; $i++) {
@@ -84,7 +84,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorum!',
                 'price' => rand(79999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(7);
         }        
     }
 }
