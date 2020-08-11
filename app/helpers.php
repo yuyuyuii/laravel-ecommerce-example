28b38dd8286563470829d0d5ada20c3b8e600399
y.maeda <yuyuyu.mae606@gmail.com>
2020-08-11 15:20:51 +0900

    とりあえず商品の在庫表示

--- app/helpers.php
+++ app/helpers.php
@@ -19,4 +19,24 @@ function productImage($path)
     //   return asset('img/noimage.png');
   }
 
+function getStock($quantity)
+{ 
+  //アラートを出す個数
+  $stock_Num = 5;
+
+  //商品の在庫が5こ以上あったら
+  if($quantity > $stock_Num){
+    $stock = '<div class="badge badge-success">在庫あり</div>';
+
+  }elseif($quantity <= $stock_Num && $quantity > 0 ){// 少ししかなかったら
+    $stock = '<div class="badge badge-warning">残りわずか</div>';
+
+  }else{
+  // 在庫がなかったら
+  $stock = '<div class="badge badge-danger">在庫なし</div>';        
+  }
+
+  return $stock;
+}  
+
 ?>
